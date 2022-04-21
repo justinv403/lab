afbc75dd6a7acde5c6ad08c56df7d54d077f5dfb
Justin <justinv403@gmail.com>
2022-04-21 02:24:59 +0000

    Multiple Edits
    Edited classes.py to fix some spacing problems, and added type hinting. Added a new testing program test_classes.py to make sure the classes.py program works even after edits.

--- classes.py
+++ classes.py
@@ -5,15 +5,15 @@ class Television:
     MIN_VOLUME = 0      # Minimum TV volume
     MAX_VOLUME = 2      # Maximum TV volume
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initialization of the TV object
         """
-        self.__channel = Television.MIN_CHANNEL
-        self.__volume = Television.MIN_VOLUME
-        self.__status = False
+        self.__channel: int = Television.MIN_CHANNEL
+        self.__volume: int = Television.MIN_VOLUME
+        self.__status: bool = False
 
-    def power(self):
+    def power(self) -> None:
         """
         Toggles the power state of the TV object
         """
@@ -24,7 +24,7 @@ class Television:
             self.__status = True
 
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
         Increases the channel of the TV, and goes back to the minimum channel if it is already at its max
         """
@@ -34,7 +34,7 @@ class Television:
             elif self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
         Decreases the channel of the TV, and goes to the max if the channel is already at the minimum value
         """
@@ -44,7 +44,7 @@ class Television:
             elif self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
         Increases the volume by 1 as long as the max has not already been reached
         """
@@ -53,7 +53,7 @@ class Television:
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
         Decreases the volume by 1 as long as the min has not already been reached
         """
@@ -61,7 +61,7 @@ class Television:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         This method returns the TV status using the format shown in the comments of main.py
 
